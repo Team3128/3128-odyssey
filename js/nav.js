831c3ceae799449d72efd68e55c5debd10edbee4
Audrey <audrey3zheng@gmail.com>
2025-06-02 17:14:48 -0700

    setup react app

--- js/nav.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const navToggle = document.getElementById("nav-toggle");
-    const navMenu = document.getElementById("nav-menu");
-
-    navToggle.addEventListener("click", function () {
-        this.classList.toggle("active");
-        navMenu.classList.toggle("active");
-    });
-});
--- js/odyssey.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("scroll").addEventListener("click", function () {
-        document.getElementById("project_menu").scrollIntoView({ behavior: "smooth" });
-    });
-});
