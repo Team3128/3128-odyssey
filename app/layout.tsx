2ce859086f559bd48ee6d52908744663b977a0b3
dhruvbantval <109187072+dhruvbantval@users.noreply.github.com>
2025-12-09 09:12:17 -0800

    Switched clientpolyfills to be true client file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import "../lib/dom-polyfills";
 import { ClientPolyfills } from "./ClientPolyfills";
 
 const geistSans = Geist({
